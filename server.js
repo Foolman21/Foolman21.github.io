629b5e58052857805cd03ca4f1ac01c3563e909a
Foolman21 <88800428+Foolman21@users.noreply.github.com>
2023-04-11 19:45:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,75 @@
+const express = require('express');
+const app = express();
 const http = require('http');
+const server = http.createServer(app);
 const WebSocket = require('ws');
 
-const server = http.createServer();
 const wss = new WebSocket.Server({ server });
 
 const clients = new Map();
+const conversations = {};
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
+  const url = new URL(req.url, 'https://example.com');
+  const username = url.searchParams.get('username');
+  const friend = url.searchParams.get('friend');
+  const conversationKey = getConversationKey(username, friend);
+
+  // Create a new conversation if it doesn't exist
+  if (!conversations[conversationKey]) {
+    conversations[conversationKey] = [];
+  }
+
+  // Add the client to the conversation
+  conversations[conversationKey].push(ws);
+
+  // Set up the event listener for incoming messages
   ws.on('message', (data) => {
     const messageData = JSON.parse(data);
-    const { from, to, message } = messageData;
+    const friendSocket = getFriendSocket(messageData.from, messageData.to);
+    if (friendSocket) {
+      friendSocket.send(JSON.stringify({
+        from: messageData.from,
+        message: messageData.message
+      }));
+    }
+  });
 
-    if (!clients.has(to)) {
-      return;
+  // Set up the event listener for closing the connection
+  ws.on('close', () => {
+    // Remove the client from the conversation
+    const index = conversations[conversationKey].indexOf(ws);
+    if (index !== -1) {
+      conversations[conversationKey].splice(index, 1);
     }
 
-    const friendSocket = clients.get(to);
-    friendSocket.send(JSON.stringify({
-      from: from,
-      message: message
-    }));
+    // If there are no more clients in the conversation, delete it
+    if (conversations[conversationKey].length === 0) {
+      delete conversations[conversationKey];
+    }
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.use(express.static('public'));
+
+const listener = server.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
+
+function getConversationKey(user1, user2) {
+  return [user1, user2].sort().join('_');
+}
+
+function getFriendSocket(from, to) {
+  const conversationKey = getConversationKey(from, to);
+  const conversation = conversations[conversationKey];
+  if (!conversation) {
+    return null;
+  }
+  for (const socket of conversation) {
+    if (socket.readyState === WebSocket.OPEN) {
+      return socket;
+    }
+  }
+  return null;
+}
